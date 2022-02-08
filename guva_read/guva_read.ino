13959b6c33a802004d4271b93207ca2f21f7c727
brandon-gamble <62863465+brandon-gamble@users.noreply.github.com>
2022-02-08 14:01:02 -0500

    Update guva_read.ino

--- guva_read/guva_read.ino
+++ guva_read/guva_read.ino
@@ -1,22 +1,27 @@
-int guva = A0;
+/*
+R2  - read sensor value as int instead of float
+    - divide by 1023 instead of 1024
+    -
 
+*/
 void setup() 
 {
   Serial.begin(9600);
+  
 }
  
 void loop() 
 {
   float sensorVoltage; 
-  float sensorValue;
- 
-  sensorValue = analogRead(guva);
-  sensorVoltage = sensorValue/1024*5;
-  Serial.print("sensor reading = ");
+  float sensorValue; // THIS SHOULD BE AN INT
+  
+  sensorValue = analogRead(A0);
+  sensorVoltage = sensorValue/1023*5;
+//  Serial.print("sensor reading = ");
   Serial.print(sensorValue);
   Serial.println("");
-  Serial.print("sensor voltage = ");
-  Serial.print(sensorVoltage);
-  Serial.println(" V");
-  delay(1000);
+//  Serial.print("sensor voltage = ");
+//  Serial.print(sensorVoltage);
+//  Serial.println(" V");
+  delay(5);
 }
