ca1ca2035e8573d9ac71b64943e22c44b3d48249
brandon-gamble <62863465+brandon-gamble@users.noreply.github.com>
2022-02-08 14:00:23 -0500

    Create guva_read.ino

---
+++ guva_read/guva_read.ino
@@ -0,0 +1,22 @@
+int guva = A0;
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+  float sensorVoltage; 
+  float sensorValue;
+ 
+  sensorValue = analogRead(guva);
+  sensorVoltage = sensorValue/1024*5;
+  Serial.print("sensor reading = ");
+  Serial.print(sensorValue);
+  Serial.println("");
+  Serial.print("sensor voltage = ");
+  Serial.print(sensorVoltage);
+  Serial.println(" V");
+  delay(1000);
+}
